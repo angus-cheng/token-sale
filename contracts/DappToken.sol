de3c837443d8d83a00357ddd54944260ee796797
Angus <anguscheng389@gmail.com>
2021-05-17 19:24:08 +1000

    2. Added name, symbol, standard and balanceOf ERC-20 functions.

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,14 +1,22 @@
 // implement ERC-20, govern crypto behaviour
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.0;
+pragma solidity >=0.5.16;
 
 contract DappToken {
-    // Constructor - 
-    // Set the total numberof tokens
-    // Read the total number of tokens
-    uint256 public totalSupply;
+    string public name = 'DAppToken';
+    string public symbol = 'DAPP';
+    string public standard = 'DApp Token v1.0';
+    uint256 public totalSupply; // state variable
 
-    constructor () public {
-        totalSupply = 1000000;
+    // mapping: key-value pair
+    mapping(address => uint256) public balanceOf;
+
+    // Constructor - set the total numberof tokens
+    // underscores for local variables
+    constructor (uint256 initialSupply) public {
+        balanceOf[msg.sender] = initialSupply; // msg = global variable. sender = address/account that deployed the contract
+        totalSupply = initialSupply;
+        
+        // allocate the initial supply
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,13 +1,32 @@
 const DappToken = artifacts.require("DappToken.sol")
 
 contract('DappToken', function(accounts) {
+    var tokenInstance;
 
-    it('sets the total supply upon deployment', function() {
+    it('initialises the contract with the correct values', function() {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.name();
+        }).then(function(name) {
+            assert.equal(name, 'DAppToken', 'has the correct name');
+            return tokenInstance.symbol();        
+        }).then(function(symbol) {
+            assert.equal(symbol, 'DAPP', 'has the correct symbol');
+            return tokenInstance.standard();
+        }).then(function(standard) {
+            assert.equal(standard, 'DApp Token v1.0', 'has the correct standard');
+        });
+    })
+
+    it('allocates the initial supply upon deployment', function() {
         return DappToken.deployed().then(function(instance) {
             tokenInstance = instance;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply) {
             assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+            return tokenInstance.balanceOf(accounts[0]);
+        }).then(function(adminBalance) {
+            assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account')
         });
     });
 })
