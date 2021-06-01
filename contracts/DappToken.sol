ecd7a1c6f467f6235f4175024226ce172d5babd6
Angus <anguscheng389@gmail.com>
2021-06-01 16:36:15 +1000

    Completed transferFrom function in DappToken.sol as well as tests for the aforementioned

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -66,7 +66,26 @@ contract DappToken {
     }
 
     // transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        // Require _from has enough tokens
+        require(_value <= balanceOf[_from]);
 
+        // Require allowance has sufficient tokens
+        require(_value <= allowance[_from][msg.sender]);
+
+        // Change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // Update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        // Call Transfer event to show a transfer happened
+        emit Transfer(_from, _to, _value);
+
+        // return a boolean
+        return true;
+    }
     // allowance
     
     // transfer
